d4474524508bd0b751bc126efa59b79de619487f
Lucas de Oliveira <121697343+lucas-oliveirs@users.noreply.github.com>
2025-09-13 19:43:36 -0300

    Update Exercicio-Apache.sh

--- Exercicio-Apache.sh
+++ Exercicio-Apache.sh
@@ -1,19 +1,38 @@
+cd / # voltando a raiz
 apt-get update # atualiza a lista de pacotes
-apt-get install -y samba apache2 # instala o Samba e o Apache se necessário
+apt-get install apache2 # instala o Samba e o Apache se necessário
 systemctl status apache2 # verifica o status do serviço Apache
-ip -br a # exibe os endereços IP das interfaces de rede
-systemctl status smbd # verifica o status do serviço Samba
+ip -br a # ip da máquina
 
-cd /var/www/
-mkdir ndominio # cria o diretório ndominio
-cd ndominio
-nano index.html # cria um arquivo HTML simples
+cd /var/www/html # indo a pasta html para criar uma página
+mkdir nportifolio # criando meu diretório de portifólio
+cd nportifolio # mudando de pasta
 
-cd /etc/apache2/sites-available/
+cp 000-default.conf nportifolio.conf # copiando o arquivo default de configuração 
 
-cp 000-default.conf ndominio.conf # copia o arquivo de configuração padrão
+<VirtualHost *:80>
+    # O site vai responder em todas as interfaces (*) na porta 80 (HTTP)
+    ServerName serv1.nportifolio.local
 
-nano -l ndominio.conf # edita o arquivo de configuração para o novo domínio
+    # E-mail do administrador (usado em mensagens de erro do Apache)
+    ServerAdmin root@nportifolio.local
 
-a2ensite ndominio.conf # habilita o novo site
-systemctl reload apache2 # recarrega o serviço Apache para aplicar as mudanças
+    # Pasta onde ficam os arquivos do site (index.html, CSS, imagens etc.)
+    DocumentRoot /var/www/html/nportifolio
+
+    # Nome alternativo para acessar o site (além do ServerName)
+    ServerAlias nportifolio
+
+    # Caminho do log de erros específicos deste VirtualHost
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+
+    # Caminho do log de acessos, usando o formato combinado (mais detalhado)
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+
+a2ensite.conf # ativando o site
+
+systemctl reload apache2 # reiniciando o serviço
+systemctl status apache2 
+
+
